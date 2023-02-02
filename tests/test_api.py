c7f2ea5e53c36885fd1d10c623102601fa5aedb5
Renee Shiyan Liu <reneesyliu@gmail.com>
2023-02-01 16:49:20 -0800

    fixed test input format

--- tests/test_api.py
+++ tests/test_api.py
@@ -10,16 +10,16 @@ lessthan50KPayload = {
   "workclass": "State-gov",
   "fnlgt": 77516,
   "education": "Bachelors",
-  "education_num": 13,
-  "marital_status": "Never-married",
+  "education-num": 13,
+  "marital-status": "Never-married",
   "occupation": "Adm-clerical",
   "relationship": "Not-in-family",
   "race": "White",
   "sex": "Male",
-  "capital_gain": 2174,
-  "capital_loss": 0,
-  "hours_per_week": 40,
-  "native_country": "United-States"
+  "capital-gain": 2174,
+  "capital-loss": 0,
+  "hours-per-week": 40,
+  "native-country": "United-States"
 }
 
 morethan50KPayload = {
