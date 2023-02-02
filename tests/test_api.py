cef43e78c2b8335abe04f5a8430e1ceb87a9a51b
Renee Shiyan Liu <reneesyliu@gmail.com>
2023-02-02 10:56:01 -0800

    fixed a typo in test_post_correct

--- tests/test_api.py
+++ tests/test_api.py
@@ -57,7 +57,7 @@ def test_get():
 def test_post_correct(lessthan50KPayload_fixture):
     res = testApp.post("/predict", json=lessthan50KPayload_fixture)
     assert res.status_code == 200
-    assert res.json()["prediction"] == "Income < 50k"
+    assert res.json()["prediction"] == "Income <= 50k"
 
 def test_post_wrong(morethan50KPayload_fixture):
     res = testApp.post("/predict", json=morethan50KPayload_fixture)
